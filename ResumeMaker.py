ea4961480a59f081e3a2aefe5a87f43f9751cf81
pvajmal29 <pvajmal29@gmail.com>
2023-02-02 13:40:52 +0530

    fix error

--- ResumeMaker.py
+++ ResumeMaker.py
@@ -2,6 +2,8 @@ import os
 import json
 import streamlit as st
 import docx
+import pdfkit
+
 
 class CreateResume:
     def __init__(self):
@@ -59,55 +61,46 @@ class CreateResume:
 
 def main():
     resume = CreateResume()
-    data = resume.load_data()
     st.title("Resume Generator")
     options = ["Basic Details", "Experience", "Academic info", "Skills and Achievements"]
     selected_option = st.radio("Select Category", options)
 
     # Get user inputs
     if selected_option == "Basic Details":
-        data["Name"] = st.text_input("Enter your name:")
-        data["Address"] = st.text_input("Enter your address:")
-        data["Phone"] = st.text_input("Enter your phone number:")
-        data["Email"] = st.text_input("Enter your email:")
-        data["LinkedIn"] = st.text_input("Enter your LinkedIn ID:")
-        resume.save_data(data)
+        resume.data["Name"] = st.text_input("Enter your name:")
+        resume.data["Address"] = st.text_input("Enter your address:")
+        resume.data["Phone"] = st.text_input("Enter your phone number:")
+        resume.data["Email"] = st.text_input("Enter your email:")
+        resume.data["LinkedIn"] = st.text_input("Enter your LinkedIn ID:")
     elif selected_option == "Experience":
-        data["JOBTITLE"] = st.text_input("Enter your job title:")
-        data["COMPANY"] = st.text_input("Enter your company name:")
-        data["ExpPlace"] = st.text_input("Enter your experience place:")
-        data["ExpDuration"] = st.text_input("Enter your experience duration:")
-        data["Objective"] = st.text_input("Enter your Career objective:")
-        data["job description"] = st.text_input("Enter your job description:")
-        resume.save_data(data)
+        resume.data["JOBTITLE"] = st.text_input("Enter your job title:")
+        resume.data["COMPANY"] = st.text_input("Enter your company name:")
+        resume.data["ExpPlace"] = st.text_input("Enter your experience place:")
+        resume.data["ExpDuration"] = st.text_input("Enter your experience duration:")
+        resume.data["Objective"] = st.text_input("Enter your Career objective:")
+        resume.data["job description"] = st.text_input("Enter your job description:")
     elif selected_option == "Skills and Achievements":
-        data["Technical Skills"] = st.text_input("Enter your technical skills:")
-        data["SSkills"] = st.text_input("Enter your soft skills:")
-        data["Achievement"] = st.text_input("Enter your achievements:")
-        resume.save_data(data)
+        resume.data["Technical Skills"] = st.text_input("Enter your technical skills:")
+        resume.data["SSkills"] = st.text_input("Enter your soft skills:")
+        resume.data["Achievement"] = st.text_input("Enter your achievements:")
     elif selected_option == "Academic info":
-        data["College"] = st.text_input("Enter your college name:")
-        data["Degree"] = st.text_input("Enter your degree:")
-        data["CollegePlace"] = st.text_input("Enter your college place:")
-        data["CGPA"] = st.text_input("Enter your CGPA:")
-        data["CollegeDetails"] = st.text_input("Enter your college details:")
-        data["CollegeDuration"] = st.text_input("Enter your college duration:")
-        resume.save_data(data)
-
-    template_file = current_dir / "resume_template.docx"
+        resume.data["College"] = st.text_input("Enter your college name:")
+        resume.data["Degree"] = st.text_input("Enter your degree:")
+        resume.data["CollegePlace"] = st.text_input("Enter your college place:")
+        resume.data["CGPA"] = st.text_input("Enter your CGPA:")
+        resume.data["CollegeDetails"] = st.text_input("Enter your college details:")
+        resume.data["CollegeDuration"] = st.text_input("Enter your college duration:")
 
     if st.button('Generate Resume'):
-        
-        resume.create_resume(template_file, data)
-        resume_file = current_dir / str('output/' + data['Name'] + '_Resume.docx')
-        with open(resume_file, "rb") as word_file:
-            word_byte = word_file.read()
-        st.download_button(
-            label=" 📄 Download Word Document",
-            data=word_byte,
-            file_name=resume_file.name,
-            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        )
+        template_file = current_dir / "resume_template.docx"
+        resume.create_resume(template_file)
+        st.success("Resume generated successfully!")
+        pdf_file = os.path.join("output", f"{resume.data['Name']}_Resume.pdf")
+        if st.button("Download PDF"):
+            resume.convert_docx_to_pdf(file_path, pdf_file)
+            with open(pdf_file, "rb") as f:
+                st.write(f.read(), unsafe_allow_html=True)
+    resume.save_data()
 
 if __name__ == "__main__":
 
